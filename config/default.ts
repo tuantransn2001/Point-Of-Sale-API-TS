b08be3aff7bd153a6f0a53831607407a2df50628
Tuấn Trần <tuantransn2001@gmail.com>
2023-04-22 09:30:04 +0700

    update feature: config price(21/4/2023)

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  host: "localhost",
+  port: 8000,
+  root_url: "/mhk-api/v2/",
+};
--- dist/routers/seed-data-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const seedRouter = (0, express_1.Router)();
-const middlewares_1 = require("../middlewares");
-const { SeedDataController } = require("../controller/seed-data-controller");
-seedRouter.post("/start", middlewares_1.authenticate, middlewares_1.authorize, SeedDataController.start);
-seedRouter.delete("/reset", middlewares_1.authenticate, middlewares_1.authorize, SeedDataController.reset);
-exports.default = seedRouter;
